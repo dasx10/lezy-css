072a85ede154f40298590f5fef6eb09699bedbe4
developer <dasx10@gmail.com>
2021-01-29 14:43:03 +0200

    speed and rewrite media query

--- media.js
+++ media.js
@@ -1,59 +1,67 @@
-const {readFile, mkdirSync, statSync} = require("fs");
+const {readFile, mkdir, stat} = require("fs").promises;
 const {pushFile} = require('./pushFile');
 const mediaPatern = /@media+[^@]+}+([\s]|)+}/g; // патерн поиска всех медиа выражений и его инструкций
-const mediaQueryPatern = /@media+[^\}]+\)/;
+const mediaQueryPatern = /@media+[^\{]+/;
 const lastClipPattern = /}+([^\w^\d}]|)+}/;
 
 let arrStyle = '(()=>{let m=['
 
-splitMQ=(filesName, dirname = './')=>{
+splitMQ= async (filesName, dirname = './') => {
     try {
-        statSync(dirname);
+        await stat(dirname);
         console.log('file or directory exists');
     }
     catch (err) {
         if (err.code === 'ENOENT') {
-            mkdirSync(dirname);
+            await mkdir(dirname);
+            console.log('make directory');
         }
     }
-    return new Promise((resolve,reject)=>readFile(filesName,"utf8",(err, data)=>{
-        console.time('Handling media queries');
-        let arrMedias = data.match(mediaPatern);
-        let all = data;
-        if(arrMedias){
-            for(let i=0;i<arrMedias.length;i++){
-                let name = arrMedias[i]
+
+    let data = await readFile(filesName,"utf8")
+    console.time('Handling media queries');
+    let arrRequest = [];
+    let arrMedias = data.match(mediaPatern);
+    // console.log(arrMedias);
+    let all = data;
+    if(arrMedias){
+        for(let i=0;i< arrMedias.length ;i++){
+            let name = '';
+            try{
+                name = arrMedias[i]
                 .match(mediaQueryPatern)[0]
                 .replace('@media','')
-    
-                pushFile(
-                    name,arrMedias[i]
-                    .split(mediaQueryPatern)[1]
-                    .replace('{','')
-                    .replace(lastClipPattern,'}'),
-                    dirname
-                );
-    
-                all = all.replace(arrMedias[i],'');
-                arrStyle += `"${name.trim()}",`;
+            }catch{
+                console.log(arrMedias[i]);
             }
+            
+
+            arrRequest.push(pushFile(
+                name,arrMedias[i]
+                .split(mediaQueryPatern)[1]
+                .replace('{','')
+                .replace(lastClipPattern,'}'),
+                dirname
+            ));
+
+            all = all.replace(arrMedias[i],'');
+            arrStyle += `"${name.trim()}",`;
         }
-        pushFile('all',all,dirname);
-        arrStyle += '"all"];let a=()=>m.forEach((s,i)=>{if(matchMedia(m[i]).matches){let l=document.createElement("link");l.rel="stylesheet";l.media=s;l.href=`test/${s.replace(/(\\(|\\)|:| )/g,"")}.css`;m.splice(i,1);document.head.append(l);}});a();addEventListener("resize",a);})();'
-        console.timeEnd('Handling media queries');
-        setTimeout(()=>{
-            console.log('\n\n');
-            console.log("\x1b[32m",`Embed code to your script file`);
-            console.log("\x1b[33m");
-            console.log(arrStyle,"\x1b[0m");
-            console.log('\n\n');
-            console.log("\x1b[32m",`or embed code to your index.html file - after opening a tag`,"\x1b[34m",`<body>`);
-            console.log("\x1b[33m");
-            console.log(`<script>${arrStyle}<script>`,"\x1b[0m");
-            console.log('\n\n');
-            resolve(arrStyle);
-        },1000);
-    }));
-}
+    }
+    arrRequest.push(pushFile('all', all, dirname));
+    arrStyle += '"all"];let a=()=>m.forEach((s,i)=>{if(matchMedia(m[i]).matches){let l=document.createElement("link");l.rel="stylesheet";l.media=s;l.href=`test/${s.replace(/(\\(|\\)|:| )/g,"")}.css`;m.splice(i,1);document.head.append(l);}});a();addEventListener("resize",a);})();'
+    await Promise.all(arrRequest)
+    console.timeEnd('Handling media queries');
+    console.log('\n\n');
+    console.log("\x1b[32m",`Embed code to your script file`);
+    console.log("\x1b[33m");
+    console.log(arrStyle,"\x1b[0m");
+    console.log('\n\n');
+    console.log("\x1b[32m",`or embed code to your index.html file - after opening a tag`,"\x1b[34m",`<body>`);
+    console.log("\x1b[33m");
+    console.log(`<script>${arrStyle}<script>`,"\x1b[0m");
+    console.log('\n\n');
+    return (arrStyle);
+}   
 
 module.exports = splitMQ;
--- pushFile.js
+++ pushFile.js
@@ -1,18 +1,18 @@
-const fs = require("fs");
+const fs = require("fs").promises;
 let issets = [];
-module.exports.pushFile=(name, data, dirname = './')=>{
+module.exports.pushFile = (name, data, dirname = './') => {
     console.log('Processing request: ', name);
-    name = name
-    .replace(/(\(|\)|:| )/g,'');
+    name = name.replace(/(\(|\)|:| )/g,'');
+
     if(issets.includes(name)){
         console.log(`Duplicate found: `,name);
-        fs.appendFile(`${name}.css`,data, function(error){
+        return fs.appendFile(`${name}.css`,data, function(error){
             if(error) throw error; // если возникла ошибка  
             console.log(`Asynchronous recording of a take to a file ${name}.css complited.`);
         });
     }else{
         issets.push(name); // сбор пройденых запросов
-        fs.writeFile(`${dirname}/${name}.css`,data, function(error){
+        return fs.writeFile(`${dirname}/${name}.css`,data, function(error){
             if(error) throw error; // если возникла ошибка
             console.log(`Writing a file asynchronously ${name}.css complited.`);
         });
